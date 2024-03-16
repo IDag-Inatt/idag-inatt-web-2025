138866d2d547a65daa6feaf40f1d1d14798dd03b
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-03-16 19:12:17 +0100

    Add mobile menu

--- js/index.js
+++ js/index.js
@@ -1,14 +1,44 @@
+// ---------- Mobile menu ----------
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu');
+const icon = menuButton.querySelector('.menu-button__icon');
+
+menuButton.addEventListener('click', () => {
+  menu.toggleAttribute('data-open');
+  menuButton.toggleAttribute('data-open');
+
+  // Update icon in menu button
+  if (icon.textContent === 'menu') {
+    icon.textContent = 'close';
+  } else {
+    icon.textContent = 'menu';
+  }
+});
+
+// When clicking a link in the menu, close the menu
+const menuLinks = document.querySelectorAll('.menu__link');
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.removeAttribute('data-open');
+    menuButton.removeAttribute('data-open');
+    icon.textContent = 'menu';
+  });
+});
+
+// ---------- Sponsor expand button ----------
+
 const expandButtons = document.querySelectorAll('.expand-button');
 
 expandButtons.forEach((button) => {
+  // When clicking the button, expand or collapse the sponsor
   button.addEventListener('click', () => {
     const sponsor = button.closest('.sponsors-section__sponsor');
-    // If sponsor has data-expanded attribute, remove it. Otherwise, add it.
     if (sponsor.hasAttribute('data-expanded')) {
       sponsor.removeAttribute('data-expanded');
       button.querySelector('.expand-button__text').textContent = 'Visa mer';
     } else {
-      sponsor.setAttribute('data-expanded', '');
+      sponsor.setAttribute('data-expanded');
       button.querySelector('.expand-button__text').textContent = 'Visa mindre';
     }
   });
